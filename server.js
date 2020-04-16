b25488c9ac0a24ed508c309bcf1b800598efee56
RichardHpa <richard.hpa@hotmail.com>
2020-04-16 20:53:40 +1200

    set up socket messages for user login and logout

--- server.js
+++ server.js
@@ -12,7 +12,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when a client connects
 io.on('connection', socket => {
-    console.log('new web socket connection');
+    // console.log('new web socket connection');
+
+    // This will emit to only the user who connects
+    socket.emit('message', 'Welcome to ChatCord!');
+
+    // Broadcasts when a user connects
+    // This will emit to everyone except the user you connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // Runs when a user disconnects
+    socket.on('disconnect', () => {
+        // this will emit to every user
+        io.emit('message', 'A user has left the chat');
+    })
+
 });
 
 const PORT = 3000 || process.env.port;
